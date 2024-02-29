c980c3b44d0a060745af24333f9a698281b22e53
salahu01 <cvsalahu12@gmail.com>
2024-02-29 09:55:58 +0530

    Initial commit

---
+++ lib/feg_cli.dart
@@ -0,0 +1,10 @@
+/// feg_cli, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate feg_cli
+/// dart pub global activate feg_cli
+///
+/// # see usage
+/// feg_cli --help
+/// ```
+library feg_cli;
